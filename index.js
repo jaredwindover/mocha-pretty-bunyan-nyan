0a0f59a3f3ba9a770a39822ea73aab120e29e1bd
Jared Windover <jaredwindover@gmail.com>
2017-08-17 09:20:11 -0400

    Using options instead of config file

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
 'use strict';
 
-
 const bunyan = require('bunyan');
 const mocha = require('mocha');
 const PrettyStream = require('bunyan-prettystream');
-const fs = require('fs');
-const OPT_FILE_NAME = 'test/mochabunyan.opts';
 
-let reporter = mocha.reporters.nyan,
-    mute = false,
+let mute = false,
     level = 'trace';
 
-if (fs.existsSync(OPT_FILE_NAME)) {
-    let config = JSON.parse(fs.readFileSync(OPT_FILE_NAME));
-
-    if (config.reporter){
-    	if (mocha.reporters[config.reporter]!==undefined){
-    		// default mocha reporter ?
-    		reporter = mocha.reporters[config.reporter];
-    	}else{
-    		// try to require it 
-    		reporter = require(config.reporter);
-    	}
+let exp = function(runner, options) {
+	let reporter = mocha.reporters.nyan;
+  if (options.reporter){
+    if (mocha.reporters[options.reporter]!==undefined){
+			// default mocha reporter ?
+			reporter = mocha.reporters[options.reporter];
+    } else {
+			// try to require it
+			reporter = require(options.reporter);
     }
+  }
 
-    mute = config.mute || mute;
-    level = config.level || level;
-}
+  mute = options.mute || mute;
+  level = options.level || level;
 
-let prettyStdOut = new PrettyStream();
-prettyStdOut.pipe(process.stdout);
+	let prettyStdOut = new PrettyStream();
+	prettyStdOut.pipe(process.stdout);
 
-var _createLogger = bunyan.createLogger;
-bunyan.createLogger = function(options) {
+	var _createLogger = bunyan.createLogger;
+	bunyan.createLogger = function(options) {
     options.streams = [{
-        level: mute ? 99 : level,
-        type: 'raw',
-        stream: prettyStdOut
+      level: mute ? 99 : level,
+      type: 'raw',
+      stream: prettyStdOut
     }];
 
     return _createLogger(options);
+	};
+
+	return reporter(runner, options);
 };
 
-
-
-module.exports = reporter;
+module.exports = exp;
